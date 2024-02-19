c334646ed802025a508a1557a7d6be02fcea4a75
Layane <lane.silva.int@gmail.com>
2024-02-19 19:38:18 -0300

    alterando main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pages.ingredients as ingredients
 # Configurações
 st.set_page_config(page_title='CoZZinhe', page_icon=':fork_and_knife:', layout='centered')
 
+
 with open('C:/Users/lanes/Dropbox/PC/Documents/CRUD_cozzinhe/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
